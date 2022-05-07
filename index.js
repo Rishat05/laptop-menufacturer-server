407fa886e60d22f6ebee3c180a6e967b8c1df230
rishat <rishatrifa56@gmail.com>
2022-05-07 14:41:05 +0600

    database connected done

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgbvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log("laptop connected");
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('Running Rishat laptop store');
 });
