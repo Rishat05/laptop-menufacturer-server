4eaedaf8fffb10646f097be29def804b51e6889e
rishat <rishatrifa56@gmail.com>
2022-05-07 14:45:17 +0600

    data load done

--- index.js
+++ index.js
@@ -14,12 +14,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgbvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log("laptop connected");
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusMyTest').collection('Item');
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
